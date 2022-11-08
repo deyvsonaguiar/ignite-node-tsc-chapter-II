9df5ac6e2f07a832d6f4dbabe9dede66b06ef5f8
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-11-08 10:33:39 -0400

    Corrigindo erro de transpilação

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+import { createCourse } from "./routes";
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World!"})
-})
+const app = express();
 
-app.listen(3333, () => console.log('Server is running...'))
+app.get("/", createCourse);
 
+app.listen(3333, () => console.log("Server is running..."));
